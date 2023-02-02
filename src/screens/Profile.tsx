bca4b3a3c4c6ac7c74cb20def8400e419d96de9a
willamy388720 <willamygt@gmail.com>
2023-02-02 11:44:33 -0300

    Photo gallery

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { TouchableOpacity } from "react-native";
 import {
   Center,
   ScrollView,
@@ -6,11 +7,13 @@ import {
   VStack,
   Skeleton,
   Heading,
+  useToast,
 } from "native-base";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { TouchableOpacity } from "react-native";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
@@ -18,6 +21,46 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState(
+    "https://img.freepik.com/fotos-gratis/conceito-de-vida-rural-com-cabras-e-ovelhas-brancas_23-2149147938.jpg?w=826&t=st=1675089746~exp=1675090346~hmac=ff126446131471560a65e16f1e25cd1fb6b0083c391c9d562bd7b0d92a304f05"
+  );
+
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {
+    setPhotoIsLoading(true);
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri
+        );
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
+          return toast.show({
+            title: "Essa imagem é muito grande! Escolha uma imagem menor.",
+            placement: "top",
+            bgColor: "red.500",
+          });
+        }
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
@@ -37,13 +80,13 @@ export function Profile() {
           ) : (
             <UserPhoto
               source={{
-                uri: "https://img.freepik.com/fotos-gratis/conceito-de-vida-rural-com-cabras-e-ovelhas-brancas_23-2149147938.jpg?w=826&t=st=1675089746~exp=1675090346~hmac=ff126446131471560a65e16f1e25cd1fb6b0083c391c9d562bd7b0d92a304f05",
+                uri: userPhoto,
               }}
               alt="Foto do usuário"
               size={PHOTO_SIZE}
             />
           )}
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color={"green.500"}
               fontWeight={"bold"}
